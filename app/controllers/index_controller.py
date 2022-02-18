a5edbae3565a2fc929a365d85ef44fd065ecf71f
keke4eva <koi2@njit.edu>
2022-02-18 16:55:47 -0600

    Update my name Again

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Keren Inaboya "
+        name = "Keren O Inaboya "
         return render_template('index.html', name=name)
